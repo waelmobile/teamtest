3b09a257b28fb1db464f71e98c642e1a6ee6dbe1
waelkessentini <waelkessentini@gmail.com>
2016-06-02 02:12:22 -0400

    new commit

--- test/src/test/testproject.java
+++ test/src/test/testproject.java
@@ -4,8 +4,9 @@ public class testproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wael");
+		System.out.println("el");
 System.out.println("Hello WORD");
+int a=0;
 	}
 
 }
